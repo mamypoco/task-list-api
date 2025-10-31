8369df04f6b24e0a2d4dbe892a8d67fd955a5e47
mami <mamypoco@gmail.com>
2025-10-31 09:40:06 -0700

    updated task model and route wip

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,22 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from typing import Optional
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] 
+    def to_dict(self): 
+        task_dict = {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            # "completed_at": self.completed_at,
+            "is_complete": self.completed_at is not None
+
+        } 
+        return task_dict
+
+
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, abort, request
+from app.models.task import Task
+from ..db import db
+
+tasks_bp = Blueprint("task_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.post()
+def create_task():
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+    
+    new_task = Task(title=title, description=description)
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+        "id": new_task.id,
+        "title": new_task.title,
+        "description": new_task.description
+    }
+    return response, 201
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+    result_list = []
+
+    for task in tasks:
+        result_list.append({
+            "id": task.id,
+            "title": task.title,
+            "description": task.description
+        })
+    return result_list
+
+
+
